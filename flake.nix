3a36c6db1b336b695e7bad2590f92f9b21d335b7
Mitch Downey <mitchldowney@gmail.com>
2026-02-25 16:54:36 -0600

    Add initial Nix flake configuration and Node version specification

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Metaboost dev environment (Node 24)";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        buildInputs = with pkgs; [
+          nodejs_24
+          git
+        ];
+        shellHook = ''
+          echo "Metaboost (Node $(node -v))"
+          echo "  npm install        # install deps"
+          echo "  npm run dev:api    # API"
+          echo "  npm run dev:web    # Next.js app"
+        '';
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          inherit buildInputs shellHook;
+        };
+      }
+    );
+}
